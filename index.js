0d6968f11c9791424f9f30e8d3704dbcea64cdb1
vinacovre <viniassis@me.com>
2017-11-14 21:34:37 -0200

    making a real example

--- index.js
+++ index.js
@@ -1,59 +1,83 @@
 const express = require('express');
-const router = express.Router();
-const app = express();
+const router  = express.Router();
+const app     = express();
 
-const logger = require('./utils/logger');
-app.use(logger);
+// const logger  = require('./utils/logger');
+// app.use(logger);
 
 app.get('/', function (req, res, next) {
-	const status = 200;
-	const message = `Welcome! We are not worrying about the visual :P`;
-	res.status(status).json(message);
-	next();
+  const status = 200;
+  const message = `Bem-vindo à loja! Não se importe com o visual da página :P`;
+  res.status(status).json(message);
+  next();
 });
 
-app.get('/promise', function (req, res, next) {
-	var arg = 10;
-	var message = 'OK!';
+var readyToBuy = false;
+var itemChosen = false;
 
-	new Promise(function (resolve, reject) {
+app.get('/example', function (req, res, next) {
+  var message = 'Verifique o console ;)';
+  // FLUXO NORMAL:
+  const choiceTime    = 1000; // Escolhe o produto no segundo 1
+  const cartTime      = 3000; // Coloca no carrinho no segundo 3
+  const executionTime = 5000; // Executa a compra no segundo 5
+  const endTime       = 7000; // Termina o exemplo no segundo 7
 
-		setTimeout(function () {
+  // EXECUÇÃO DA COMPRA
+  new Promise(function (resolve, reject) {
 
-			for (let i = 0; i <= 10; i++) {
-				console.log(i + '\n');
-			}
-		
-			if (arg === 10)
-				resolve(arg);
-		}, 3000); // executes a function in a pre-defined time (ms)
+    setTimeout(function () {
+      if (readyToBuy)
+        resolve(1);
+      else
+        reject('Carrinho vazio :(\n');
+    }, executionTime); // Tente executar essa Promise entre 1 e 3 segundos
 
-	}).then(function (num) {
-		console.log('\n==> first then: ', num); return num * 2;
-	}).then(function (num) {
-		console.log('==> second then: ', num); return num * 2;
-	}).then(function (num) {
-		console.log('==> last then: ', num);
-	}).catch(function (err) {
-		console.log('==> catch: ', err);
-	});
+  }).then(function (num) {
+    console.log('\n==> (' + num + ') Verifico se está no carrinho');
+    return ++num;
+  }).then(function (num) {
+    console.log('==> (' + num + ') Obtenho informações do pagamento');
+    return ++num;
+  }).then(function (num) {
+    console.log('==> (' + num + ') Efetuo a compra');
+    return ++num;
+  }).then(function (num) {
+    console.log('==> (' + num + ') Mando para os correios\n\n');
+  }).catch(function (err) {
+    console.log('==> Compra não efetuada:', err);
+  });
 
-	new Promise(function (resolve, reject) {
+  // COLOCA NO CARRINHO
+  new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      if (itemChosen)
+        resolve();
+      else
+        reject('Item ainda não foi escolhido :(\n');
+    }, cartTime);
+  }).then(function () {
+    console.log('>>>>>>>>>>> Item foi colocado no carrinho!\n');
+    readyToBuy = true;
+  }).catch(function (err) {
+    console.log('==> Compra não efetuada:', err);
+  });
 
-		setTimeout(function () { resolve(); }, 1000);
+  // ESCOLHA DO ITEM
+  setTimeout(function () {
+    console.log('>>>>>>>>>>> Ainda escolhendo...\n');
+    itemChosen = true;
+  }, choiceTime); // Tente escolher o item depois do segundo 5 (> 5000 ms) ...
+  // ... mas antes do tempo final, certamente.
 
-	}).then(function () {
-		console.log('>>>>>>>>>>> I may be executed in the before!\n');
-	});
+  // FIM DO EXEMPLO
+  setTimeout(function () {
+    console.log("F I M\n");
+    process.exit(0);
+  }, 7500);
 
-	new Promise(function (resolve, reject) {
-				setTimeout(function () { resolve(); }, 100);
-			}).then(function () {
-				console.log('>>>>>>>>>>> When am I executed??\n');
-			});
-	
-	const status = 200;
-	res.status(status).json(message);
+  const status = 200;
+  res.status(status).json(message);
 });
 
 const port = 3000;
